cd9589da38d6b90f839065a1238892c94499d06d
QuittoGames <brunamoreiraquitto@gmail.com>
2026-08-22 16:56:41 -0300

    Lauch

---
+++ src/main/java/cannelo/marques/interdiciplinar/interdiciplinar/InterdiciplinarApplication.java
@@ -0,0 +1,13 @@
+package cannelo.marques.interdiciplinar.interdiciplinar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterdiciplinarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterdiciplinarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cannelo/marques/interdiciplinar/interdiciplinar/InterdiciplinarApplicationTests.java
@@ -0,0 +1,13 @@
+package cannelo.marques.interdiciplinar.interdiciplinar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterdiciplinarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
