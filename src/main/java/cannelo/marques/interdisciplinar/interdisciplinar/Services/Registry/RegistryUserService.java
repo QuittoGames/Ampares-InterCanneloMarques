33d5afbc0c8cf9e11b45d6e91bb256bc825b0cdb
QuittoGames <brunamoreiraquitto@gmail.com>
2026-09-05 10:29:03 -0300

    chore(cleanup): remove unused import statement in RegistryUserService

--- src/main/java/cannelo/marques/interdisciplinar/interdisciplinar/Services/Registry/RegistryUserService.java
+++ src/main/java/cannelo/marques/interdisciplinar/interdisciplinar/Services/Registry/RegistryUserService.java
@@ -4,7 +4,6 @@ import java.math.BigDecimal;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.UUID;
-import java.util.function.Function;
 
 import org.springframework.stereotype.Service;
 
